c8d35458f923d0d371a91eed68fead7940d85747
Jacob Austin <jaustin@imagesoftinc.com>
2019-11-29 13:23:02 -0500

    Added logic to route from main page to household info screens. Added ability to join a household.

--- app/src/main/java/edu/oakland/petfoodtracker/JoinHHActivity2.java
+++ app/src/main/java/edu/oakland/petfoodtracker/JoinHHActivity2.java
@@ -2,13 +2,63 @@ package edu.oakland.petfoodtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class JoinHHActivity2 extends AppCompatActivity {
+
+    SQLiteDatabase db;
+
+    EditText et_name, et_password, et_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_join_hh2 );
+
+        et_name = findViewById(R.id.URNmET);
+        et_password = findViewById(R.id.HHPWET);
+        et_id = findViewById(R.id.HHIDET);
+    }
+
+    public void joinHH(View view)
+    {
+        db = DBHelper.getDB(this);
+
+        String name = et_name.getText().toString();
+        String password = et_password.getText().toString();
+        String id = et_id.getText().toString();
+
+        Cursor res = db.rawQuery("SELECT id, name FROM household WHERE hex_code = '" + id + "' AND password = '" + password + "'", null);
+        res.moveToFirst();
+        int count = res.getCount();
+
+        if(count == 1) // We have an exact match, password is correct
+        {
+            db.execSQL("INSERT INTO user (name, household_id, manager) VALUES ('" + name + "', '" + res.getInt(0) + "', 0)");
+            Intent intent = new Intent(this, JoinHHSuccessActivity3.class);
+            intent.putExtra("user_name", name);
+            intent.putExtra("HH_name", res.getString(1));
+            startActivity(intent);
+        }
+        else if(count > 1) // This should never happen. This occurs if two households have the same hex code.
+        {
+            Toast.makeText(this, "ERROR: This hex code is not unique.", Toast.LENGTH_SHORT).show();
+        }
+        else // Either hex code is wrong or the password doesn't match.
+        {
+            Intent intent = new Intent(this, JoinHHFailActivity4.class);
+            startActivity(intent);
+        }
+
+        db.close();
     }
 }
--- app/src/main/java/edu/oakland/petfoodtracker/MainActivity.java
+++ app/src/main/java/edu/oakland/petfoodtracker/MainActivity.java
@@ -2,9 +2,11 @@ package edu.oakland.petfoodtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,14 +15,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     SQLiteDatabase db;
-    DBHelper dbHelper;
-    ListView list;
     ArrayAdapter adapter;
-
-    ArrayList<Integer> arr_id;
     ArrayList<String> arr_name;
-    ArrayList<String> arr_grade;
-    ArrayList<String> arr_date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +24,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView( R.layout.activity_main );
 
         db = DBHelper.getDB(this);
-        arr_name = new ArrayList<String>();
+        arr_name = new ArrayList<>();
 
-        Cursor res = DBHelper.queryDB(db, "SELECT * FROM household WHERE id > 0");
+        Cursor res = db.rawQuery("SELECT * FROM household WHERE id > 0", null);
+        res.moveToFirst();
         int count = res.getCount();
         if(count > 0)
-        {
-            do
-            {
+            do {
                 arr_name.add(res.getString(1));
-            } while(res.moveToNext());
-        }
+            } while (res.moveToNext());
 
         db.execSQL("DELETE FROM household WHERE hex_code = \"0001-4747\"");
 
-        list = findViewById(R.id.list);
+        //list = findViewById(R.id.list);
         adapter = new ArrayAdapter<>(this, R.layout.list_item, arr_name);
-        list.setAdapter(adapter);
+        //list.setAdapter(adapter);
 
         db.close();
+    }
+
+    public void route_joinHH(View view)
+    {
+        Intent intent = new Intent(this, JoinHHActivity2.class);
+        startActivity(intent);
+    }
+
+    public void route_createHH(View view)
+    {
+        Intent intent = new Intent(this, CreateHHActivity5.class);
+        startActivity(intent);
     }
 }
