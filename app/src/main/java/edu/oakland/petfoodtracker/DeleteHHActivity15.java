77ab9d9f62e99dad5b4e067a504c5a88fc17a319
TeenaAlex <teenaalex@oakland.edu>
2019-11-29 14:51:04 -0500

    Views complete. Fixed buttons for all the views.

--- app/src/main/java/edu/oakland/petfoodtracker/DeleteHHActivity15.java
+++ app/src/main/java/edu/oakland/petfoodtracker/DeleteHHActivity15.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MakeManagerActivity12 extends AppCompatActivity {
+public class DeleteHHActivity15 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_make_manager12 );
+        setContentView( R.layout.activity_delete_hh15 );
     }
 }
--- app/src/main/java/edu/oakland/petfoodtracker/MakeManagerActivity11.java
+++ app/src/main/java/edu/oakland/petfoodtracker/MakeManagerActivity11.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ManageMembersActivity11 extends AppCompatActivity {
+public class MakeManagerActivity11 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_manage_members11 );
+        setContentView( R.layout.activity_make_manager11 );
     }
 }
